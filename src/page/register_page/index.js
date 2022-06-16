56833c9ad9a279445fa0b7a3578d99bd755a47fb
AlFahroni <alfahroni97@gmail.com>
2022-06-16 21:26:55 +0700

    Revised folder placement and icon naming register page

--- src/page/register_page/index.js
+++ src/page/register_page/index.js
@@ -1,12 +1,10 @@
-import signupImage from "../../global_component/images/Saly-43.png";
-
 const RegisterPage = () => {
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 max-h-screen w-full">
 
             <div className="hidden sm:inline-flex flex items-center justify-center bg-sky-600 h-screen">         
-                    <img className="w-2/3 m-auto" src={signupImage}/>
+                    <img className="w-2/3 m-auto" src="./icon/icon_rocket.png" alt="/" />
             </div>
         </div>
     )
