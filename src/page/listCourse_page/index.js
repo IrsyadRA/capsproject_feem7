bd219129642b545a60c5db881a6663ed7dccd335
AlFahroni <alfahroni97@gmail.com>
2022-06-16 21:38:50 +0700

    Revised folder placement and icon naming at List Course Page

--- src/page/listCourse_page/index.js
+++ src/page/listCourse_page/index.js
@@ -1,10 +1,3 @@
-import Key from "../../global_component/images/Key.png";
-import Message from "../../global_component/images/Message.png";
-import Task from "../../global_component/images/Task.png";
-import Quiz from "../../global_component/images/Quiz.png";
-import Chart from "../../global_component/images/Chart.png";
-import Trophy from "../../global_component/images/Trophy.png";
-
 const ListCoursePage = () => {
 
     return (
@@ -13,39 +6,39 @@ const ListCoursePage = () => {
                 <p className="text-xl text-sky-500 font-bold">Course Scheme</p>
                 <h1 className="text-6xl text-black font-bold">How to Study at Ebility</h1>
             </div>
-            
+
             <div className="grid md:grid-cols-3 gap-4 px-2 text-center mt-20">
                 <div className="border py-8 rounded-xl shadow-lg">
-                    <img className="m-auto" src={Key} alt="desc-text"/>
+                    <img className="m-auto" src="./icon/icon_key.png" alt="desc-text"/>
                     <p className="text-xl text-gray-400 mt-4 font-medium-1">Access all materials on the web</p>
                 </div>
                 <div className="border py-8 rounded-xl shadow-lg">
-                    <img className="m-auto" src={Message} alt="desc-text-2"/>
+                    <img className="m-auto" src="./icon/icon_message.png" alt="desc-text-2"/>
                     <p className="text-xl text-gray-400 mt-4 font-medium">Join the discussion group</p>
                 </div>
                 <div className="border py-8 rounded-xl shadow-lg">
-                    <img className="m-auto" src={Task} alt="desc-text-3"/>
+                    <img className="m-auto" src="./icon/icon_task.png" alt="desc-text-3"/>
                     <p className="text-xl text-gray-400 mt-4 font-medium">Do all the tasks</p>
                 </div>
             </div>
 
             <div className="grid md:grid-cols-3 gap-4 px-2 text-center mt-5">
                 <div className="border py-8 rounded-xl shadow-lg">
-                    <img className="m-auto" src={Quiz} alt="desc-text-4"/>
+                    <img className="m-auto" src="./icon/icon_task.png" alt="desc-text-4"/>
                     <p className="text-xl text-gray-400 mt-4 font-medium">Do the quiz every weekend</p>
                 </div>
                 <div className="border py-8 rounded-xl shadow-lg">
-                    <img className="m-auto" src={Chart} alt="desc-text-5"/>
+                    <img className="m-auto" src="./icon/icon_chart.png" alt="desc-text-5"/>
                     <p className="text-xl text-gray-400 mt-4 font-medium">View and improve your achievements</p>
                 </div>
                 <div className="border py-8 rounded-xl shadow-lg">
-                    <img className="m-auto" src={Trophy} alt="desc-text-6"/>
+                    <img className="m-auto" src="./icon/icon_trophy.png" alt="desc-text-6"/>
                     <p className="text-xl text-gray-400 mt-4 font-medium">Get your skills and certificates</p>
                 </div>
             </div>
-            
+
         </div>
     )
 }
 
-export default ListCoursePage;
+export default ListCoursePage; 
