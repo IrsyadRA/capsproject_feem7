bbad6108364da1e0f87842083800e604a1c1fb1d
AlFahroni <alfahroni97@gmail.com>
2022-06-21 00:00:53 +0700

    Make a description of the course section on the course details

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import DetailCoursePage from './page/detailCourse';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DetailCoursePage/>
     </div>
   );
 }
---
+++ src/page/detailCourse.js
@@ -0,0 +1,23 @@
+const DetailCoursePage = () => {
+    return (
+        <div className="max-w-screen ">
+            <div className="max-w-[1280px] mx-auto">
+                <div className="grid grid-flow-col grid-cols-12">
+                    <div className="flex flex-col items-start text-left py-20 my-8 col-span-7">
+                        <h1 className="text-3xl text-black font-bold">Apa itu React JS?</h1>
+                        <p className="text-base text-gray-400 mt-4 font-medium-1">
+                            Ketika belajar tentang web development, mungkin Anda pernah mendengar tentang React JS. Istilah ini bisa jadi membuat Anda bertanya-tanya. Apa itu React JS? Fungsinya buat apa? Apa hubungannya dengan JavaScript?
+                            <br></br>
+                            <br></br>
+                            Wawasan tentang React JS adalah sesuatu yang sangat penting. Khususnya jika Anda ingin membuat User Interface (UI) website yang menarik.</p>
+                    </div>
+                    <div className="hidden sm:block col-span-8 col-start-9">
+                        
+                    </div>
+                </div>  
+            </div>
+        </div>
+        )
+}
+
+export default DetailCoursePage;
